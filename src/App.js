2bbe42dc32223b1346828127fee1fc4be3ff5fda
Tuan Hamid <tn220771@dal.ca>
2022-06-10 11:58:44 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {routes} from "./routes";
 import LayoutDealer from "./components/LayoutDealer";
+import Login from "./pages/user/Login";
 
 function App() {
   return(
       <Router>
-        <LayoutDealer>
           <Routes>
-            {routes.map((route) => (
-                <Route
-                    path={route.path}
-                    element={<route.component />}
-                />
-            ))}
+              <Route exact path="/login" element={<Login/>}/>
+              <Route path="/" element={<LayoutDealer />} >
+                  {routes.map((route) => (
+                      <Route
+                          path={route.path}
+                          element={<route.component />}
+                      />
+                  ))}
+              </Route>
           </Routes>
-        </LayoutDealer>
       </Router>
+
+      // <Router>
+      //   <LayoutDealer>
+      //     <Routes>
+      //       {routes.map((route) => (
+      //           <Route
+      //               path={route.path}
+      //               element={<route.component />}
+      //           />
+      //       ))}
+      //     </Routes>
+      //   </LayoutDealer>
+      // </Router>
   );
 }
 
--- src/components/LayoutDealer.js
+++ src/components/LayoutDealer.js
@@ -1,7 +1,8 @@
 import { Box } from "@mui/material";
 import Navbar from "./Navbar";
+import { Outlet } from 'react-router-dom';
 
-function LayoutDealer(props) {
+function LayoutDealer() {
     return(
         <Box
             sx={{
@@ -14,7 +15,7 @@ function LayoutDealer(props) {
             }}
         >
             <Navbar />
-            {props.children}
+            <Outlet/>
         </Box>
     );
 }
