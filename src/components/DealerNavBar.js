98ae9c16050e41df3929cae72557b47aad6352a3
Tuan Hamid <tn220771@dal.ca>
2022-06-10 14:38:53 -0300

    nav fixes

--- src/components/DealerNavBar.js
+++ src/components/DealerNavBar.js
@@ -17,11 +17,33 @@ import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import {Link as navLink} from "react-router-dom";
 
-const pages = ["Vehicles", "Accessories", "Repairs", "Services"];
-const settings = ["Account", "Logout"];
+const pages = [{
+    name: "Vehicles",
+    link: "/manage/vehicles"
+}, {
+    name: "Accessories",
+    link: "/manage/accessories"
+}, {
+    name: "Repairs",
+    link: "/manage/repairs"
+}, {
+    name: "Services",
+    link: "/manage/services"
+}, {
+    name: "Employees",
+    link: "/manage/employee"
+}];
+const settings = [{
+    name: "Account",
+    link: "/manage/employee"
+}, {
+    name: "Logout",
+    link: "/login"
+}];
 
-function DealerNavBar () {
+function DealerNavBar() {
 
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -54,7 +76,7 @@ function DealerNavBar () {
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
                         <DirectionsCarIcon
-                            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+                            sx={{display: {xs: "none", md: "flex"}, mr: 1}}
                         />
                         <Typography
                             variant="h6"
@@ -63,7 +85,7 @@ function DealerNavBar () {
                             href="/"
                             sx={{
                                 mr: 2,
-                                display: { xs: "none", md: "flex" },
+                                display: {xs: "none", md: "flex"},
                                 fontFamily: "monospace",
                                 fontWeight: 700,
                                 letterSpacing: ".3rem",
@@ -74,7 +96,7 @@ function DealerNavBar () {
                             LOGO
                         </Typography>
 
-                        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                        <Box sx={{flexGrow: 1, display: {xs: "flex", md: "none"}}}>
                             <IconButton
                                 size="large"
                                 aria-label="account of current user"
@@ -83,7 +105,7 @@ function DealerNavBar () {
                                 onClick={handleOpenNavMenu}
                                 color="inherit"
                             >
-                                <MenuIcon />
+                                <MenuIcon/>
                             </IconButton>
                             <Menu
                                 id="menu-appbar"
@@ -100,18 +122,18 @@ function DealerNavBar () {
                                 open={Boolean(anchorElNav)}
                                 onClose={handleCloseNavMenu}
                                 sx={{
-                                    display: { xs: "block", md: "none" },
+                                    display: {xs: "block", md: "none"},
                                 }}
                             >
                                 {pages.map((page) => (
-                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center">{page}</Typography>
+                                    <MenuItem key={page} onClick={handleCloseNavMenu} component={navLink} to={page.link}>
+                                        <Typography textAlign="center">{page.name}</Typography>
                                     </MenuItem>
                                 ))}
                             </Menu>
                         </Box>
                         <DirectionsCarIcon
-                            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+                            sx={{display: {xs: "flex", md: "none"}, mr: 1}}
                         />
                         <Typography
                             variant="h5"
@@ -120,7 +142,7 @@ function DealerNavBar () {
                             href=""
                             sx={{
                                 mr: 2,
-                                display: { xs: "flex", md: "none" },
+                                display: {xs: "flex", md: "none"},
                                 flexGrow: 1,
                                 fontFamily: "monospace",
                                 fontWeight: 700,
@@ -131,26 +153,25 @@ function DealerNavBar () {
                         >
                             LOGO
                         </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                        <Box sx={{flexGrow: 1, display: {xs: "none", md: "flex"}}}>
                             {pages.map((page) => (
-                                <Button
-                                    key={page}
-                                    onClick={handleCloseNavMenu}
-                                    sx={{ my: 2, color: "white", display: "block" }}
+                                <Button component={navLink} to={page.link}
+                                    key={page.name}
+                                    sx={{my: 2, color: "white", display: "block"}}
                                 >
-                                    {page}
+                                    {page.name}
                                 </Button>
                             ))}
                         </Box>
 
-                        <Box sx={{ flexGrow: 0 }}>
+                        <Box sx={{flexGrow: 0}}>
                             <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
                                 </IconButton>
                             </Tooltip>
                             <Menu
-                                sx={{ mt: "45px" }}
+                                sx={{mt: "45px"}}
                                 id="menu-appbar"
                                 anchorEl={anchorElUser}
                                 anchorOrigin={{
@@ -166,8 +187,8 @@ function DealerNavBar () {
                                 onClose={handleCloseUserMenu}
                             >
                                 {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center">{setting}</Typography>
+                                    <MenuItem key={setting.name} onClick={handleCloseUserMenu} component={navLink} to={setting.link}>
+                                        <Typography textAlign="center">{setting.name}</Typography>
                                     </MenuItem>
                                 ))}
                             </Menu>
@@ -175,11 +196,11 @@ function DealerNavBar () {
                     </Toolbar>
                 </Container>
             </AppBar>
-            <Divider />
+            <Divider/>
             <div className="thumb-nav">
                 <Stack spacing={2}>
                     <Breadcrumbs
-                        separator={<NavigateNextIcon fontSize="small" />}
+                        separator={<NavigateNextIcon fontSize="small"/>}
                         aria-label="breadcrumb"
                     >
                         {breadcrumbs}
