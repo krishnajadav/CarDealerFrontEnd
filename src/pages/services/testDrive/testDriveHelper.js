90380902d3f9888a69282b1c9b107feee6d692d9
Leah Isenor <leah.isenor@dal.ca>
2022-07-15 05:06:50 -0300

    integrating delete and update test drive

--- src/pages/services/testDrive/testDriveHelper.js
+++ src/pages/services/testDrive/testDriveHelper.js
@@ -2,11 +2,11 @@
 Author: Leah Isenor, B00316891
 */
 const axios = require("axios");
-const url = "http://localhost:4200"
+const { baseURL } = require("./../helper");
 
 export const getTimes = async () => {
     try {
-        const times = await axios.get(url+"/api/testdrives/timeslots");
+        const times = await axios.get(baseURL+"/testdrives/timeslots");
         times.data.unshift("Select");
         return times.data;
     } catch (err) {
@@ -16,7 +16,7 @@ export const getTimes = async () => {
 
 export const getCars = async () => {
     try {
-        const cars = await axios.get(url+"/api/testdrives/cars");
+        const cars = await axios.get(baseURL+"/testdrives/cars");
         cars.data.unshift("Select");
         return cars.data;
     } catch (err) {
@@ -26,7 +26,7 @@ export const getCars = async () => {
 
 export const getUnavailableTimes = async () => {
     try {
-        const unavailable = await axios.get(url+"/api/testdrives/appointments/unavailable");
+        const unavailable = await axios.get(baseURL+"/testdrives/appointments/unavailable");
         return unavailable.data;
     } catch (err) {
         console.log(err.message);
@@ -35,15 +35,13 @@ export const getUnavailableTimes = async () => {
 
 export const bookTestDrive = async (data) => {
     try {
-        console.log("booking test drive")
-        console.log(data)
         const body = {
             car: data.carModel,
             date: data.date,
             time: data.timeSlot
         }
         let id = localStorage.getItem("id");
-        await axios.put(url +"/api/testdrives/appointments/"+id, body);
+        await axios.put(baseURL +"/testdrives/appointments/"+id, body);
         return "Successfully booked test drive";
     } catch (err) {
         console.log(err.message);
@@ -54,8 +52,8 @@ export const bookTestDrive = async (data) => {
 export const getTestDrives = async () => {
     try {
         let id = localStorage.getItem("id");
-        let appointments = await axios.get(url +"/api/testdrives/appointments/"+id);
-        return appointments;
+        let appointments = await axios.get(baseURL +"/testdrives/appointments/"+id);
+        return appointments.data;
     } catch (err) {
         console.log(err.message);
     }
@@ -63,7 +61,7 @@ export const getTestDrives = async () => {
 
 export const cancelTestDrive = async (id) => {
     try {
-        await axios.delete(url +"/api/testdrives/appointments/cancel/"+id);
+        await axios.delete(baseURL +"/testdrives/appointments/cancel/"+id);
         return "Successfully canceled test drive";
     } catch (err) {
         console.log(err.message);
