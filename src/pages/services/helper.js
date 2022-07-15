f3e7e7feb9898f86ba0138a5f21b72c112f552c1
Leah Isenor <leah.isenor@dal.ca>
2022-07-15 01:02:23 -0300

    Removed test drive from dealer view

--- src/pages/services/helper.js
+++ src/pages/services/helper.js
@@ -27,12 +27,6 @@ export const services = [
     image: testDriveCar,
     content: "Select from available vehicles to book a guaranteed time for a test drive"
   },
-  {
-    service: "Repair Estimate",
-    image: repairsCar,
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  },
 ];
 
 export const carModels = [
